6b74af61ed37ef4485e71f47d467ec07503e1876
George Davitiani <46586741+gdmoney@users.noreply.github.com>
2024-12-23 13:37:18 -0800

    .

--- code/count_sets.py
+++ code/count_sets.py
@@ -17,7 +17,7 @@ def calculate_total_sets(totals):
     return sum(totals)
 
 if __name__ == "__main__":
-    file_path = '/Users/gdavitiani/Documents/GitHub/fitness/README.md'  # Adjust the path if necessary
+    file_path = 'README.md'  # Adjust the path if necessary
     lines = read_readme(file_path)
     totals = extract_set_totals(lines)
     total_sets = calculate_total_sets(totals)
